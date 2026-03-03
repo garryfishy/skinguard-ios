efcff80edebff62de025958b2c9d514bb29c4dab
Garry Agassi <agassi.garry@gmail.com>
2026-03-03 10:46:57 +0700

    Initial Commit

---
+++ SkinGuard/SkinGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinGuardApp.swift
+//  SkinGuard
+//
+//  Created by Fishy on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SkinGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
